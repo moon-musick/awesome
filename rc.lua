e4607d653e9dd3ce3ce8d414bae3b9ccb035a18d
Łukasz Górski <moon.musick@gmail.com>
2015-07-21 22:51:37 +0200

    Change date display format

--- rc.lua
+++ rc.lua
@@ -142,7 +142,7 @@ mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
 
 -- {{{ Wibox
 -- Create a textclock widget
-mytextclock = awful.widget.textclock({ align = "right" })
+mytextclock = awful.widget.textclock({ align = "right" }, " %a %Y-%m-%d · %H:%M ")
 
 -- Create a systray
 mysystray = widget({ type = "systray" })
